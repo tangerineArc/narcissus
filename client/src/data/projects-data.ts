fa5ebb9c035d02e9592141f7ee0ed935d8994677
Swagatam <swagatamheroz@outlook.com>
2025-06-02 11:23:08 +0530

    chore: add projects and education data

---
+++ client/src/data/projects-data.ts
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    title: "Asphodel",
+    url: "https://github.com/tangerineArc/asphodel",
+    description:
+      "a two-pass assembler and emulator based on a custom instruction set ",
+  },
+  {
+    title: "Résumé Builder",
+    url: "https://github.com/tangerineArc/resume-builder",
+    description:
+      "create, preview and print résumés on the fly using an easy-to-use WYSIWYG editor",
+  },
+  {
+    title: "Aeolus",
+    url: "https://github.com/tangerineArc/aeolus",
+    description:
+      "frontend client for displaying weather-data from Visual Crossing",
+  },
+  {
+    title: "Battleship",
+    url: "https://github.com/tangerineArc/battleship",
+    description: "play the battleship game in the browser",
+  },
+];
