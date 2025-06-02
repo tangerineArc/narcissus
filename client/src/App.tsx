1ad58ec1319483b85b010b7e702aae58d84dce61
Swagatam <swagatamheroz@outlook.com>
2025-06-02 11:21:50 +0530

    feat: add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
 
-function App() {
-  return <>"Hello"</>;
+import AboutPage from "./pages/AboutPage.tsx";
+import BlogPage from "./pages/BlogPage.tsx";
+import EducationPage from "./pages/EducationPage.tsx";
+import MiscPage from "./pages/MiscPage.tsx";
+import ProjectsPage from "./pages/ProjectsPage.tsx";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<AboutPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/education" element={<EducationPage />} />
+      <Route path="/misc" element={<MiscPage />} />
+      <Route path="/blog" element={<BlogPage />} />
+    </Routes>
+  );
 }
-
-export default App;
