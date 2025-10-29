390a394db2d0b9bbcbdac7d5a97ebb80975e129f
Swagatam Pati <swagatam.pati.2104@gmail.com>
2025-10-30 00:35:47 +0530

    temporary change: add nlp pdf

--- client/src/pages/PdfPage.tsx
+++ client/src/pages/PdfPage.tsx
@@ -1,6 +1,5 @@
 import Footer from "../components/Footer.tsx";
 import Navbar from "../components/Navbar.tsx";
-import UnderConstruction from "../components/UnderConstruction.tsx";
 
 export default function PdfPage() {
   return (
